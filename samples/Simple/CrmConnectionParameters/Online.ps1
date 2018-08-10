dcf77c368cefec69e1c025e63b2231d3376342c6
Alan Mervitz <amervitz@users.noreply.github.com>
2018-08-09 19:21:06 -0600

    Update samples for TLS 1.2 usage with Dynamics 365 v9.x

--- samples/Simple/CrmConnectionParameters/Online.ps1
+++ samples/Simple/CrmConnectionParameters/Online.ps1
@@ -1,5 +1,9 @@
 # a connection to a Dynamics 365 Online deployment
 # update the values as appropriate
+
+# uncomment this when connecting to Dynamics 365 v9.x to ensure TLS 1.2 is used
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 @{
     OrganizationName = "orgfab51ca3" # see Settings > Customizations > Developer Resources > "Unique Name"
     ServerUrl = "https://fabrikam.crm.dynamics.com" # the online organization URL
