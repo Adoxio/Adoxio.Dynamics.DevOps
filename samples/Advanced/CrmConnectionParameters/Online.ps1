846ccae517386d8271976d411a8e2a41c7345877
Alan Mervitz <amervitz@users.noreply.github.com>
2018-11-12 16:59:32 -0600

    Update Online.ps1 sample connection parameter files to use TLS 1.2

--- samples/Advanced/CrmConnectionParameters/Online.ps1
+++ samples/Advanced/CrmConnectionParameters/Online.ps1
@@ -1,8 +1,8 @@
 # a connection to a Dynamics 365 Online deployment
 # update the values as appropriate
 
-# uncomment this when connecting to Dynamics 365 v9.x to ensure TLS 1.2 is used
-# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# ensure TLS 1.2 is used when connecting to Dynamics 365 v9.x
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 @{
     OrganizationName = "orgfab51ca3" # see Settings > Customizations > Developer Resources > "Unique Name"
